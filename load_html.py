a76096e1b0be6bf650f0d6c309d306564b2bed42
aooutput <31516990+aooutput@users.noreply.github.com>
2026-09-22 17:00:54 -0500

    add py for html

---
+++ load_html.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Read HTML file from your GitHub repository
+with open("test_colordice.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
+
+# Render the HTML in the app
+components.html(html_content)#, height=600, scrolling=True)
